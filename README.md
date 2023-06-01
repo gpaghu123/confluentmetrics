f9e4b39919dd8d57041c91a6c8828697339544c4
Raghu P <raghup@ibm.com>
2023-06-01 15:49:14 -0700

    final version of confluent metrics

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# devfile-sample-python-basic
+# This is an example of scraping metrics form Confluent Cloud and injecting them into Instana
+
+
+Confluent cloud has a well documented [metrics api](https://docs.confluent.io/cloud/current/monitoring/metrics-api.html), and already delivers metrics either in Openmetrics or JSON format. In this example, we use the OpenMetrics format. 
+
+The Confluent result has a timestamp attached to the metric, which causes some confusion with Instana. This code simply removes the timestamp and sends the Confluent result to the Instana backend. The current example uses Flask as the app mechanism and Gunicorn as the app server. Should be suficient in most instances, particularly behind firewalls. It needs more robust mechanism such as gunicorn behind Nginx if is exposed to the Web. 
+
+The test system was a Linux VM running Python, Flask and Gunicorn and the Instana agent. Since the metric scraping only happens relatively infrequently as configured in the Instana configuration, this doesnt need much infrastructure resources. **Be aware of API metering that Confluent does on their end**. 512MB of memory, 10-15GB of disk and 1-2 CPUs should be more than sufficient. No data is stored on disk. 
+
+Refer Instana documenation for how to set this up on Kubernetes. 
+
+Below is a section of the agent config file (ususally at `/opt/instana/agent/etc/instana`). Isolating the machine in its own zone may be a good idea!
+
+
+    com.instana.plugin.prometheus:
+        poll_rate: 30
+        customMetricSources:
+          - url: 'http://localhost:8000/'
+          metricNameIncludeRegex: '.*'  
+      
+
+Set up the Python venv, install modules from requirements.txt, and run:
+
+`gunicorn -w 2 'app:app'`
+
+The metrics will show up in the Instana backend, and you should be able to set up a custom dashboard to display then in a consumable format! 
--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from prometheus_client import start_http_server, Gauge, Info, make_wsgi_app
-import prometheus_client
-import time
-import base64
 import pycurl
 import certifi
 from io import BytesIO
 import sys
-import json
-import datetime
-from datetime import timedelta
 import logging
+import re
+from flask import Flask
 
 
 logging.basicConfig(
@@ -20,21 +15,31 @@ logging.basicConfig(
     ]
 )
 
-class DGMetrics:
-    def __init__(self, url, apikey):
-        self.url = url
-        self.apikey = apikey
-        self.define_prometheus_metrics()
-    
-    def retrieve_metrics_in_json(self):
-        logging.info("refreshing " + str(type(self).__name__))
+#These variables need to be changed per the actual requirement. better to put them in toml files, and to the environment
+common_url = 'https://api.telemetry.confluent.cloud/v2/metrics/cloud/export?resource.kafka.id=lkc-8w2qyq'
+token = '335YYPHRCLSMHQZD:u72RozOB7kSjW2KDr758PuB2ALopGLqM6Nq5Ri9YFt9G3aibPDYoxk5D3IQ1SHbz'
+
+def retrieve_metrics(common_url, token):
+        def remove_timestamp(s):
+            # this function removes the timestamp at the end of confluent metrics that Instana does not like. 
+            # all metrics begin with confluent, and have a fixed format that the re is matching for.  
+            # The backend would not import metrics that has a real number format. That is why the code drops the decimal
+            # and digits that succeed it. Pretty much always .0. 
+            m = re.match(r"(^confluent\S*)(\s*\S*\s*)\d*$",s) 
+            if m:
+                s = m.group(1)+" "+m.group(2)[0:-3]
+            return s
+        
+        def generate(strary):
+         for s in strary:
+            yield s + '\n'
+        logging.info("refreshing..." )
         buffer = BytesIO()
         c = pycurl.Curl()
-        customheaders = ["Authorization: ZenApiKey {0}".format(base64.b64encode(self.apikey.encode("ascii")).decode('UTF8'))]
-        c.setopt(c.URL,self.url)
-        c.setopt(c.HTTPHEADER, customheaders)
-        c.setopt(c.SSL_VERIFYPEER, 0)
-        c.setopt(c.SSL_VERIFYHOST, 0)
+        myurl = common_url 
+        customheaders = ["Authorization:  {0}".format(token)]
+        c.setopt(c.URL,myurl)
+        c.setopt(c.USERPWD,token)
         c.setopt(c.CUSTOMREQUEST, "GET")
         c.setopt(c.WRITEDATA, buffer)
         c.setopt(c.CAINFO, certifi.where())
@@ -42,131 +47,20 @@ class DGMetrics:
             c.perform()
         except:
             print(sys.exc_info())
+            logging.CRITICAL(sys.exc_info())
         finally:
             c.close()
-        jsonstr = json.loads(buffer.getvalue().decode('iso-8859-1'))
-        if jsonstr["resStatus"] == "SUCCESS":
-            return(jsonstr["result"])
-        else:
-            raise(Exception("Failed to get metrics"+str(type(self).__name__)))
+        logging.info("received response.")
+        # retrieve the bytes, decode to UTF and convert to string array; then remove the timestamp and generate a new array
+        strary1 = list(map(remove_timestamp,buffer.getvalue().decode("utf-8").splitlines()))
+        # attach the new line characer to each response line to make it more human readable. Access the endpoint from any browser
+        # to see the result
+        return app.response_class(generate(strary1), mimetype='text')
 
-    def define_prometheus_metrics(self):
-        self.result = self.retrieve_metrics_in_json()
+# start the app to serve responses to mertics requests
 
-    def set_prometheus_metrics(self):
-        self.result = self.retrieve_metrics_in_json()
-
-class InstanceMetrics(DGMetrics):
-    def define_prometheus_metrics(self):
-        super().define_prometheus_metrics()
-        ns = 'DGInstance'
-        subsys = self.result["cloneEngineName"]+"_"+self.result["locationName"]
-        self.result = self.result["tableCloneStatus"]
-        self.metricAry = {}
-        for key in self.result:
-            self.metricAry[key]=Gauge(key,'',namespace=ns, subsystem=subsys)
-
-    def set_prometheus_metrics(self):
-        super().retrieve_metrics_in_json()["tableCloneStatus"]
-        for  key in self.metricAry:
-            self.metricAry[key].set(self.result[key])
-
-
-# this is a general purpose generator to identify only the metrics we need. As below, any class that has no subclass is a metric we want.  
-def get_only_derived_classes(cls):
-	for subclass in cls.__subclasses__():
-		if not subclass.__subclasses__():
-			yield subclass
-		yield from get_only_derived_classes(subclass)
-
-class TableMetrics(DGMetrics):
-    # Since we are not retrieving all the table metrics returned, define the ones we want
-    # To retrieve the metric from DG and send to Instana, define a subclass of BaseMetric, and call the init with the key, name and description
-    #     then, override the getValue method to specify any special processing required as shown below. 
-    #     All values returned by getValue should be numeric, since Instana only takes numerics from Prometheus. 
-    #     I did not have much luck with "info" type metrics
-
-
-
-    class BaseMetric:
-        def __init__(self,index,mname,mdesc):
-            self.index = index
-            self.mname = mname
-            self.mdesc = mdesc
-        def getvalue(self,item):
-            self.value = item[self.index]
-    class mReplicationStatus (BaseMetric):
-        def __init__(self):
-            super().__init__("replicationStatus","Replicating","Table replication1=Enabled")
-        def getValue(self,item):
-            super().getvalue(item)
-            return 1 if self.value == "Enabled" else 0
-    class mTableSize(BaseMetric):
-        factor_table = {"MB": 1/1e3, "GB": 1.0, "TB: 1": 1e3}
-        def __init__(self):
-            super().__init__("tableSize","TableSize","Table size in GB")
-        def getValue(self,item):
-            super().getvalue(item)
-            try: 
-               return float(self.value[:-2]) * self.factor_table[self.value[-2:]] #converted to GB
-            except(e):
-                raise Exception("Unexpected size "+self.value)
-    class BaseTime(BaseMetric):
-         def getValue(self, item):
-            super().getvalue(item)
-            return (datetime.datetime.now() - datetime.datetime.fromtimestamp(self.value/1e3))/timedelta(minutes=1)       
-    class mLastLoadTime(BaseTime):
-        def __init__(self):
-            super().__init__("lastLoadTime","lastLoad","Minutes since last load")
-    class mLastReplTime(BaseTime):
-        def __init__(self):
-            super().__init__("lastReplicationTime","lastReplication","Minutes since last replicate")
-
-    
-    # Instantiate only the lowest level classes in the heirarchy. These represent the metrics we want to capture and send to Instana
-    def __init__(self, url, apikey): 
-        self.tableMetricstoGet  = [subclass() for subclass in get_only_derived_classes(self.BaseMetric)]
-        super().__init__(url,apikey)
-    # this sets up the metrics with uniqie names in the Prometheus client. This is a requirement for Prometheus. 
-    # Each TableID is a row, 1 column per metric holding a Guage object and a subclass instance of BaseMetric
-    def define_prometheus_metrics(self):
-        super().define_prometheus_metrics()
-        ns = 'DGTable'
-        self.nameAryTable = {item["id"]: 
-                                [[Gauge(metric.mname,metric.mdesc,namespace=ns, subsystem=item["id"]), metric] for metric in self.tableMetricstoGet] 
-                             for item in self.result
-                            }
-
-    def set_prometheus_metrics(self):
-        super().set_prometheus_metrics()   
-        try: 
-            [[gaugeary[0].set(gaugeary[1].getValue(item)) for gaugeary in self.nameAryTable[item["id"]]] for item in self.result]
-        except Exception as e:
-            raise Exception("Cant process input metrics json",e,self.result)
-
-# main program starts here
-
-common_url = "https://cpd-zen1.apps.dgsvt5.cp.fyre.ibm.com/icp4data-databases/dg-1672018833030580/zen1/clone_system/"
-token = "admin:Uh68bJcGJithBQl87Q9bp1SW0jZ0kfwht4ZV6u45"
-
-instance_metrics = InstanceMetrics(common_url+"clone_engine/status?force_refresh=true",token)
-table_metrics = TableMetrics(common_url+"clone_engine/clone_table",token)
-
-# this turns off all the python run-time related metrics. We only want to send DG metrics to Instana
-prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
-prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
-prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
-
-if __name__ == '__main__':
-    # Start up the prometheus metric server to expose the metrics.
-    # I had problems with any port other than 8080. The Instana agent seems to prefer this port
-    start_http_server(8080)
-
-while True:
-    # Refresh the metrics every 270 seconds, since frequency at which the Instana agent sends to the backend is 300 seconds
-    try:
-        instance_metrics.set_prometheus_metrics()
-        table_metrics.set_prometheus_metrics()
-    except Exception as e:
-        logging.critical("Refresh failed" % e)
-    time.sleep(250)
+app = Flask(__name__)
+#you can change the endpoint below
+@app.route("/")
+def get_metrics():
+    return(retrieve_metrics(common_url,token))
