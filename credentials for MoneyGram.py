7e015ae3d956121ad40add2378d17b6aa48f090d
gpaghu123 <99775827+gpaghu123@users.noreply.github.com>
2024-07-02 22:16:30 -0700

    Update credentials for MoneyGram.py

--- credentials for MoneyGram.py
+++ credentials for MoneyGram.py
@@ -1,3 +1,3 @@
 #credentials for MoneyGram
 common_url = 'https://api.telemetry.confluent.cloud/v2/metrics/cloud/export?resource.kafka.id=lkc-zyq5d'
-token = 'U4YQPIG3FY5VGIVP:qawBUHP865DS0oIx9imTDpzkS0BuCCRjTG0CyKNrj20DWT07Zd73dw2vjnpuS646'
+token = ''
